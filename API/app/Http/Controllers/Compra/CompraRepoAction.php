3cd377245524a3a174f6d75bee67b9d015fc057f
Noe.Rivera <noe.rivera@idtconsultores.com>
2022-05-31 12:29:09 -0500

    Adds functionality to api

--- API/app/Http/Controllers/Compra/CompraRepoAction.php
+++ API/app/Http/Controllers/Compra/CompraRepoAction.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Compra;
-
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -9,21 +8,52 @@ class CompraRepoAction
 {
     public function create(Request $request)
     {
-        $compras = DB::table('compras')->insert([
+        $productos = $request->productos;
+        $insert = [ 'serie' => 'PR'];
+        foreach ( $productos as $producto){
+            $totalProductos = $producto['cantidad'] + $producto['cantidad'];
+            $subtotal = $producto['cantidad'] * $producto['precio'];
+            $subtotalIva = $subtotal + $subtotal * 0.16;
+            $total = $total + $subtotalIva;
+        }
+
+
+        $compras = DB::table('compras')->insert(array_merge($insert,[
+            'folio'                                      => DB::raw( "(SELECT 
+                                                                            COALESCE ( MAX ( folio ) , 0 ) + 1
+                                                                            FROM
+                                                                                    compras 
+                                                                                        )"),
+            'serie_folio'                               => DB::raw( "(
+                                                                    dSELECT 
+                                                                        CONCAT (
+                                                                             '{$insert['serie']}', '-', 
+                                                                            ( SELECT LPAD ( ( COALESCE ( MAX ( folio ), 0 ) +1 )::TEXT, 5 , '0') FROM compras ) 
+                                                                        )
+                                                                    )"),
+
             'compra_id' => $request->compra_id,
-            'nombre' => $request->nombre,
-            'ubicacion_primaria' => $request->ubicacion_primaria,
-            'ubicacion_secundaria' => $request->ubicacion_secundaria,
-            'folio' => $request->folio,
-            'responsable_nombre' => $request->responsable_nombre,
-            'registro_autor_id' => $request->registro_autor_id,
-            'registro_fecha' => $request->registro_fecha
+            'fecha_vencimiento' => $request->fecha_vencimiento,
+            'factura_folio' => $request->factura_folio,
+            'descripcion' => $request->descripcion,
+
+                                                                        ]));
+
+        $compras = DB::table('compras_detalle')->insert([
+            'descripcion' => $request->descripcion,
+            'productos' => $productos,
         ]);
+    
+        
+
+   
+
         if ($compras == 1) {
             echo "compra registrado exitosamente";
         } else {
             echo "error al registrar compra";
         }
+        
         return $compras;
     }
 
